a048efe6950e5bce91d5a5fef5ee0377383f29ce
Mr. Ian <ianloron00@gmail.com>
2021-01-18 17:26:01 -0300

    small, useless changes

--- src/model/StringException.java
+++ src/model/StringException.java
@@ -2,11 +2,11 @@ package model;
 
 public class StringException extends Exception {
     public StringException (String str) {
-        super("Problemas com a entrada das informacoes: " +  str);
+        super("Problemas com a entrada das informações: " +  str);
     }
     public static class EmptyStringException extends StringException {
         public EmptyStringException() {
-            super("Você nao pode deixar espacos vazios.");
+            super("Você não pode deixar espaços vazios.");
         }
     }
     public static class SignException extends StringException {
